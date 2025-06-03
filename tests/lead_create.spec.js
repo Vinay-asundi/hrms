fd68472cc1affefc7fc1abe117d32fed1988ad1b
Vinay-asundi <vinayasundivinay@gmail.com>
2025-06-03 16:52:11 +0530

    Initial commit

---
+++ tests/lead_create.spec.js
@@ -0,0 +1,31 @@
+// tests/create_lead.spec.js
+const { test, expect } = require('@playwright/test');
+const { getAuthToken } = require('../utils/authUtils');
+
+test('POST /lead/create with auth token', async ({ request }) => {
+  const token = await getAuthToken(request); 
+
+  const response = await request.post('lead/create', {
+    data: {
+      name: 'test1',
+      orgType: 'test1',
+      ownerName: {
+        firstName: 'vinay',
+        lastName: 'a'
+      },
+      mobile: '99999999999'
+    },
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    }
+  });
+
+  const body = await response.json();
+  console.log("Create Lead Response:", JSON.stringify(body, null, 2));
+
+  expect(response.status()).toBe(200);
+    expect(body).toHaveProperty('status', 200);
+   expect(body).toHaveProperty('message', 'Lead Added Successfully');
+  
+});
